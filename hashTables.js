e95de7e7ab453354b55f11616a0869bc682d1549
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-01-22 14:44:44 +0300

    hash tables

--- hashTables.js
+++ hashTables.js
@@ -1,1 +1,27 @@
 /* HASH TABLES IN DSA */
+const hash = (string, max) => {
+	const hash = 0;
+	for(let i = 0; i < string.lenth; i++){
+		hash += string.charCodeAt(i);
+	}
+	return hash % max;
+}
+
+let hashTable = function(){
+	let storage = [];
+	const storageLimit = 4;
+	this.print = function(){
+		console.log(storage);
+	}
+	this.add = function(key, value){
+		const index = hash(ey, storageLimit);
+		if(storage[index] === undefined){
+			storage[index] = [
+				[key, value]
+			]
+		}
+		else{
+			const inserted = false;
+		}
+	}
+}
